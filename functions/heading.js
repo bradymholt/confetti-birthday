89dfcd2c6636a15ca4d64e35fb4f6d0411f39250
Brady Holt <brady.holt@gmail.com>
2020-05-15 08:47:47 -0500

    More testing functions

--- functions/heading.js
+++ functions/heading.js
@@ -1,13 +1,14 @@
-const headingMap = new Map([["030a66ac", "Happy Birthday Brady!"]]);
+const queryNameMap = new Map([["030a66ac", "Brady"]]);
 
 exports.handler = function (event, context, callback) {
-  const heading = headingMap.get("030a66ac");
+  const name = queryNameMap.get(queryStringParameters["id"]);
+  const heading = `Happy Birthday ${name}!`;
   callback(null, {
     statusCode: 200,
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ heading, event }),
+    body: JSON.stringify({ heading }),
   });
 };
