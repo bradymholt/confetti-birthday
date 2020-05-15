0d3cdc04693e57c038047d708fe5922d16ecc23e
Brady Holt <brady.holt@gmail.com>
2020-05-15 08:39:07 -0500

    More testing functions

---
+++ functions/heading.js
@@ -0,0 +1,15 @@
+const headingMap = {
+  "030a66ac": "Happy Birthday Brady!",
+};
+
+exports.handler = function (event, context, callback) {
+  const heading =  headingMap("030a66ac");
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ heading, event }),
+  });
+};
