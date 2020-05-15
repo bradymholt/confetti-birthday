921a0f856ae8764f385b1ae8efe55d08c3717617
Brady Holt <brady.holt@gmail.com>
2020-05-15 08:49:41 -0500

    More testing functions

--- functions/heading.js
+++ functions/heading.js
@@ -1,7 +1,7 @@
 const queryNameMap = new Map([["030a66ac", "Brady"]]);
 
 exports.handler = function (event, context, callback) {
-  const name = queryNameMap.get(event.queryStringParameters["id"]);
+  const name = queryNameMap.get(event.queryStringParameters["id"]) || "";
   const heading = `Happy Birthday ${name}!`;
   callback(null, {
     statusCode: 200,
