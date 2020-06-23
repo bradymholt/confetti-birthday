c9ab33447c3d3b5bc83199227e4cf901cf5fae57
Brady Holt <brady.holt@gmail.com>
2020-06-23 17:56:58 -0500

    New birthdays

--- functions/message.js
+++ functions/message.js
@@ -7,7 +7,10 @@ const queryMessageMap = new Map([
   ["2acbeccb", "Happy Birthday Daniel!"],
   ["556bc12f", "Happy Birthday Sarah!"], // Sarah S.
   ["a86d69af", "Happy Birthday Kody!"],
-  ["3bf4499e", "Happy Birthday Emily!"]
+  ["3bf4499e", "Happy Birthday Emily!"],
+  ["9de5321c", "Happy Birthday Matt!"],
+  ["ef691596", "Happy Birthday Jazzi!"],
+  ["7ec4e75c", "Happy Birthday Graham!"], // Graham Chance
 ]);
 
 exports.handler = function (event, context, callback) {
